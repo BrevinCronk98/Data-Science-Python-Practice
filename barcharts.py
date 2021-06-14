adcc39d58499d158ecd5348c07f8ee37f1bcae59
brevin <cronkbrevin98@gmail.com>
2021-06-13 17:55:51 -0700

    Working on new skills with data in Python

---
+++ barcharts.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+from collections import Counter
+
+# #Movie Bar Chart
+# movies = ["Annie Hall", "Ben-Hur", "Casablanca", "Gahndi", "West Side Story"]
+# num_oscars = [5, 11, 3, 8, 10]
+
+# # plot bars with left x-coordinates [0, 1, 2, 3, 4], heights [num_oscars]
+# plt.bar(range(len(movies)), num_oscars)
+
+# plt.title("My Favorite Movies")  # Add a title
+# plt.ylabel("# of Academy Awards") # Label the Y-axis
+
+# #label x-axis with movie names at bar centers
+# plt.xticks(range(len(movies)), movies)
+
+# plt.show()
