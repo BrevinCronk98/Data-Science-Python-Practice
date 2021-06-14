ae0639ac0c5746ffae765eae27df0ba28a3e28a4
brevin <cronkbrevin98@gmail.com>
2021-06-13 20:47:27 -0700

    Done Studying for the day

---
+++ Chapter4-LinearAlgebra/vectors.py
@@ -0,0 +1,12 @@
+from typing import List
+
+Vector = List[float]
+
+height_weight_age = [70, # Height
+                     170, # Weight
+                     40]  # Age
+grades = [95,  # Exam 1
+          80,  # Exam 2
+          75,   # Exam 3
+          62 ] # Exam 4
+                
